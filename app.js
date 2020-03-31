98db0b0505506e90f21740f5aa2d248431d22e4f
Armou <aoudin@student.42.fr>
2020-03-31 19:50:41 +0200

    commit la part2

--- app.js
+++ app.js
@@ -10,20 +10,23 @@ function filterArrayByTitle(object, string) {
 function searchPosts() {
     var xhttp = new XMLHttpRequest();
     var searchTerm = document.getElementById('search-term').value;
+    var apiKey = 'b5db1638ca8c42e78b791c3a29971cc7'
+
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var newObj = JSON.parse(this.responseText);
-            newObj = filterArrayByTitle(newObj, searchTerm);
+            console.log(newObj)
+            // newObj = filterArrayByTitle(newObj, searchTerm);
             var finalHTML = "<h2>Here are the results : </h2>";
-            if (newObj.length > 0) {
-                newObj.forEach((value) => {
-                    finalHTML += '<h4>Post #' + value.id + '</h4>' +
-                                    '<ul><li><b>Title</b> : ' + value.title + '</li>' +
-                                    '<li><b>Content</b> : ' + value.body + '</li></ul>';
+            if (newObj.articles.length > 0) {
+                newObj.articles.forEach((value) => {
+                    finalHTML += '<h4>Article Title : ' + value.title + '</h4>' +
+                                    '<ul><li><b>Description</b> : ' + value.description + '</li>' +
+                                    '<li><b>URL</b> : <a target="_blank" href=' + value.url + '>' + value.url + '</a></li></ul>';
                 });
             } else {
-                finalHTML = "<h2>Sorry, we couldn't find any posts with this search term \"" + searchTerm + "\". Try with another word.</h2>"
+                finalHTML = "<h2>Sorry, we couldn't find any article with this search term \"" + searchTerm + "\". Try with another word.</h2>"
             }
             document.getElementById('search-results').innerHTML = finalHTML;
 
@@ -31,7 +34,8 @@ function searchPosts() {
             // console.log('error');
         }
     }
-
-    xhttp.open('GET', 'https://jsonplaceholder.typicode.com/posts/');
+    var url = 'https://newsapi.org/v2/everything?q=' + searchTerm +  '&pageSize=50&language=fr&apiKey=' + apiKey;
+    console.log(url)
+    xhttp.open('GET', url);
     xhttp.send();
 }
